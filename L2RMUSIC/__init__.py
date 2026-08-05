50b5e14d92a641fbdbf74096b060342c059578ee
ASHISHTIME <155893081+BWFTIME@users.noreply.github.com>
2026-08-06 03:13:00 +0530

    Update __init__.py

--- L2RMUSIC/__init__.py
+++ L2RMUSIC/__init__.py
@@ -1,10 +1,14 @@
+# ✅ Corrected __init__.py
+
+from .logging import LOGGER
+from .config import config   # agar config variable hai
+
+# Ab baaki sab import karo
 from L2RMUSIC.core.bot import Ashish
 from L2RMUSIC.core.dir import dirr
 from L2RMUSIC.core.git import git
 from L2RMUSIC.core.userbot import Userbot
 from L2RMUSIC.misc import dbb, heroku
-
-from .logging import LOGGER
 
 dirr()
 git()
@@ -13,7 +17,6 @@ heroku()
 
 app = Ashish()
 userbot = Userbot()
-
 
 from .platforms import *
 
